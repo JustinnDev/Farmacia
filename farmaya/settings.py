ed1beef0fbce50e524737affa0bddf2a98b2fa49
JustinnDev <justinalmaodev@gmail.com>
2025-11-01 16:34:32 -0400

    Config settings.py

--- farmaya/settings.py
+++ farmaya/settings.py
@@ -119,13 +119,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
